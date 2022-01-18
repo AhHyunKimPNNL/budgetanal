63aee1842eee9e76714fbb8377b5c0460f62a3b3
AhHyunKimPNNL <ahhyun.kim@pnnl.gov>
2022-01-18 13:39:43 -0800

    Deployed 59c91ea with MkDocs version: 1.2.3

---
+++ img/convert.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+filedir=/compyfs/www/kima320/Budget_Analysis/Cloud_regime/
+filename=Vertical_profiles_by_region_and_chkpt_in_one_page
+savedir=/compyfs/www/kima320/png_plot/Budget_Analysis/
+#filedir=/compyfs/www/kima320/pictures/q_budget_rad_compy_FC5AV1C-L_ne30_maint-1.0_cnd_diag1.1/
+#savedir=/qfs/people/kima320/git_work/budgetanal/docs/02_cloud_regime/img/
+#filename=seasonal_changes_Precipitation
+#filename=All_zonal_by_variable_and_add_substeps
+#filename=All_vertical_integral_by_variable_and_add_substeps
+#filename=All_global_mean
+#filename=All_vertical_integral_by_chkpt_and_add_substeps
+#filename=All_zonal_by_chkpt_and_add_substeps
+#filename=All_profiles
+
+convert -density 800 ${filedir}${filename}.pdf -resize 25% ${savedir}${filename}.png
+nfiles=`find ${savedir}${filename}*.png |wc -l`
+ifile=0
+echo number of files are $nfiles
+
+while [ $ifile -lt $nfiles ]
+do
+  changefile=${savedir}${filename}-${ifile}.png
+  convert ${changefile} -flatten -fuzz 1% -trim +repage ${changefile} 
+  ((ifile++))
+done
+#convert -density 800 $filename.pdf -resize 25% $filename.png

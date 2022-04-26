186a2db9b9c40903ae653279106cd78ceacb992b
AhHyunKimPNNL <ahhyun.kim@pnnl.gov>
2022-04-26 09:20:18 -0700

    Deployed 6e9ca7b with MkDocs version: 1.2.3

--- img/convert.sh
+++ img/convert.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #filedir=/compyfs/www/kima320/pictures/cosp_compy_FC5AV1C-L_ne30_maint-1.0_cnd_diag1.1/
-cases=test
-casename=rad_test_3h_compy_FC5AV1C-L_ne30_gmd_2020_330_with_forcing_method
-#casename=rad_${cases}_compy_FC5AV1C-L_ne30_gmd_2020_330_with_forcing_method
-#filedir=/compyfs/www/kima320/pictures/$casename/
-filedir=/compyfs/www/kima320/pictures/$casename/multiple_plot/
+cases=noclubbsub
+#casename=rad_test_3h_compy_FC5AV1C-L_ne30_gmd_2020_330_with_forcing_method
+casename=rad_${cases}_compy_FC5AV1C-L_ne30_gmd_2020_330_with_forcing_method
+filedir=/compyfs/www/kima320/pictures/$casename/
+#filedir=/compyfs/www/kima320/pictures/$casename/multiple_plot/
 #filedir=/compyfs/www/kima320/Budget_Analysis/With_forcing_method/Difference/
 #filedir=/compyfs/www/kima320/Budget_Analysis/Cloud_regime/
 #filedir=/compyfs/www/kima320/pictures/q_budget_rad_compy_FC5AV1C-L_ne30_maint-1.0_cnd_diag1.1/
-filename="Diur_cy_diff_of_Turbulence_0th"
+filename="pressure_slice_Moisture_adv_850"
 #filename=Vertical_profiles_by_region_and_chkpt_in_one_page
 #savedir=/compyfs/www/kima320/png_plot/Budget_Analysis/03_Sc_Cu/noclubbrad/
-#savedir=/compyfs/www/kima320/png_plot/Budget_Analysis/03_Sc_Cu/${cases}/
-savedir=/compyfs/www/kima320/png_plot/Budget_Analysis/02_zm_rad_modification/${cases}/
+savedir=/compyfs/www/kima320/png_plot/Budget_Analysis/03_Sc_Cu/${cases}/
+#savedir=/compyfs/www/kima320/png_plot/Budget_Analysis/02_zm_rad_modification/${cases}/
 #savedir=/qfs/people/kima320/git_work/budgetanal/docs/02_cloud_regime/img/
 mkdir $savedir
 
